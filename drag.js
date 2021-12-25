f2705333323ed85e8c6f96a1a2ca5fd56c41c080
Lucas Sannier <40730498+fox3000foxy@users.noreply.github.com>
2021-12-25 21:20:33 +0000

    Change some images and add StreamChat

--- drag.js
+++ drag.js
@@ -1,4 +1,19 @@
+if(!localStorage.camHide) localStorage.camHide = false
 function dragElement(elmnt) {
+  var atRight = false;
+  if(localStorage.atRight) atRight = JSON.parse(localStorage.atRight)
+  var atBottom = false;
+  if(localStorage.atBottom) atBottom = JSON.parse(localStorage.atBottom)
+  //console.log('atRight:',atRight,'atBottom:',atBottom)
+  if(atRight==true) 
+  {
+    newLeft = (window.innerWidth - elmnt.offsetWidth) - ((20/100*window.innerWidth) + 20)
+    elmnt.style.left = newLeft + "px";
+  }
+  if(atBottom==true) {
+    newTop = (window.innerHeight - elmnt.offsetHeight) - 25
+    elmnt.style.top = newTop + "px";
+  }
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
     // if present, the header is where you move the DIV from:
@@ -28,13 +43,59 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    newTop = ((elmnt.offsetTop - pos2) / window.innerHeight * 100)
+    newLeft =  ((elmnt.offsetLeft - pos1) / window.innerWidth * 100)
+    if(newTop<=0) newTop = 1
+    if(newLeft<=0) newLeft = 1
+    elmnt.style.top = newTop + "%";
+    elmnt.style.left = newLeft + "%";
   }
 
   function closeDragElement() {
     // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
+    if(newLeft <= 40) {
+      atRight = false
+      newLeft = 25
+    }
+    else { 
+      atRight = true
+      newLeft = (window.innerWidth - elmnt.offsetWidth) - ((20/100*window.innerWidth) + 20)
+    }
+    if(newTop <= 40) {
+      atBottom = false
+      newTop = 25 
+    } 
+    else {
+      atBottom = true
+      newTop = (window.innerHeight - elmnt.offsetHeight) - 25
+    } 
+    elmnt.style.top = newTop + "px";
+    elmnt.style.left = newLeft + "px";
+    localStorage.atBottom = atBottom
+    localStorage.atRight = atRight
+  }
+  window.onresize = ()=>{
+    if(atRight) { 
+      newLeft = (window.innerWidth - elmnt.offsetWidth) - ((20/100*window.innerWidth) + 20)
+      elmnt.style.left = newLeft + "px";
+    }
+    if(atBottom) { 
+      newTop = (window.innerHeight - elmnt.offsetHeight) - 25
+      elmnt.style.top = newTop + "px";
+    }
+  }
+  window.onkeypress = (evt)=>{
+    if(evt.key!="c") return;
+      camHide = !JSON.parse(localStorage.camHide)
+      videoHeader.style.visibility = camHide?"hidden":"visible"
+      localStorage.camHide = camHide
   }
 }
+
+document.querySelectorAll('*[draggable]').forEach((elem)=>{
+  elmnt = elem
+  console.log(elem)
+  dragElement(elem)
+})
--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 const videoElem = document.getElementById("video");
 const logElem = document.getElementById("log");
-// const startElem = document.getElementById("start");
-// const stopElem = document.getElementById("stop");
-// Options for getDisplayMedia()
-
 var displayMediaOptions = {
   video: {
     cursor: "always",
@@ -32,6 +28,7 @@ async function startCapture() {
 }
 
 function stopCapture(evt) {
+  if(videoElem.srcObject==null) return
   let tracks = videoElem.srcObject.getTracks();
 
   tracks.forEach(track => track.stop());
@@ -47,15 +44,12 @@ function dumpOptionsInfo() {
   console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
 }
 
-startCapture()
+//startCapture()
 
-var video = document.createElement('video');
-video.setAttribute('playsinline', '');
-video.setAttribute('autoplay', '');
-video.setAttribute('muted', '');
-video.style.width = '200px';
-video.style.height = '200px';
-
+var video = document.getElementById('userCamera');
+var videoHeader = document.getElementById('userCameraheader');
+camHide = JSON.parse(localStorage.camHide)
+videoHeader.style.visibility = camHide?"hidden":"visible"
 var facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
 var constraints = {
   audio: false,
@@ -68,4 +62,13 @@ navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
   video.srcObject = stream;
 });
 
-document.body.appendChild(video)
+videoHeader.appendChild(video)
+
+function askChannel(){
+  channelID = prompt('Channel Id:','')
+  if(channelID==null) return
+  localStorage.channelID = channelID
+  document.getElementById('streamChat').src = 'https://streamchat.fox3000.repl.co/?channel='+localStorage.channelID
+}
+
+if(localStorage.channelID) document.getElementById('streamChat').src = 'https://streamchat.fox3000.repl.co/?channel='+localStorage.channelID
